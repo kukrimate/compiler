8b8c94956823f9a66c1d9230cd36a5cf6b169914
Mate Kukri <kukri.mate@gmail.com>
2021-09-11 23:04:19 +0100

    Re-work code generator

--- src/gen.rs
+++ src/gen.rs
@@ -12,7 +12,7 @@ use tempfile::tempdir;
 
 macro_rules! print_or_die {
     ($file:expr, $($args:expr),*) => {
-        writeln!($file, $($args),*).unwrap();
+        writeln!($file, $($args),*).unwrap()
     }
 }
 
@@ -65,69 +65,202 @@ fn assert_integral(dtype: &Type) {
     }
 }
 
+// Registers usable as temporary values
+// Rip, Rsp, Rbp are reserved for the compiler's use
+#[derive(Clone, Copy, Debug)]
+enum Reg {
+    Rax = (1 << 0),
+    Rbx = (1 << 1),
+    Rcx = (1 << 2),
+    Rdx = (1 << 3),
+    Rsi = (1 << 4),
+    Rdi = (1 << 5),
+    R8  = (1 << 6),
+    R9  = (1 << 7),
+    R10 = (1 << 8),
+    R11 = (1 << 9),
+    R12 = (1 << 10),
+    R13 = (1 << 11),
+    R14 = (1 << 12),
+    R15 = (1 << 13),
+}
+
+impl Reg {
+    fn to_str(&self) -> &str {
+        match self {
+            Reg::Rax => "rax", Reg::Rbx => "rbx", Reg::Rcx => "rcx",
+            Reg::Rdx => "rdx", Reg::Rsi => "rsi", Reg::Rdi => "rdi",
+            Reg::R8  => "r8" , Reg::R9  => "r9" , Reg::R10 => "r10",
+            Reg::R11 => "r11", Reg::R12 => "r12", Reg::R13 => "r13",
+            Reg::R14 => "r14", Reg::R15 => "r15"
+        }
+    }
+}
+
+// List of all registers (in allocation order, callee-saved ones come first)
+static ALL_REGS: [Reg; 14] = [
+    Reg::Rbx, Reg::R12, Reg::R13, Reg::R14, Reg::R15, // Callee saved
+    Reg::Rax, Reg::Rcx, Reg::Rdx, Reg::Rsi, Reg::Rdi, // Caller saved
+    Reg::R8,  Reg::R9,  Reg::R10, Reg::R11,
+];
+
+// List of caller saved registers
+static CALLER_SAVED: [Reg; 9] = [
+    Reg::Rax, Reg::Rcx, Reg::Rdx, Reg::Rsi, Reg::Rdi,
+    Reg::R8,  Reg::R9,  Reg::R10, Reg::R11,
+];
+
+// List of registers used for parameters
+static PARAM_REGS: [Reg; 6] = [
+    Reg::Rdi, Reg::Rsi, Reg::Rdx, Reg::Rcx, Reg::R8, Reg::R9
+];
+
+// Free register mask
+struct RegMask {
+    usedregs: u16
+}
+
+impl RegMask {
+    fn new() -> RegMask {
+        RegMask {
+            usedregs: 0
+        }
+    }
+
+    fn alloc_reg(&mut self) -> Option<Reg> {
+        for reg in ALL_REGS {
+            let mask = reg as u16;
+            if self.usedregs & mask == 0 {
+                self.usedregs |= mask;
+                return Some(reg);
+            }
+        }
+        None
+    }
+
+    fn clear_reg(&mut self, reg: Reg) {
+        let mask = reg as u16;
+        if self.usedregs & mask == 0 {
+            panic!("Register {:?} is already clear", reg);
+        }
+        self.usedregs &= !mask;
+    }
+}
+struct FuncCtx {
+    // Register allocator
+    regmask: RegMask,
+    // Local variables
+    locals: HashMap<Rc<str>, (Type, usize)>,
+    // Stack frame size
+    frame_size: usize,
+}
+
+impl FuncCtx {
+    // Find the effective address of a local variable
+    fn local_ea(&self, name: &Rc<str>) -> String {
+        let (_, offset) = self.locals.get(name).unwrap();
+        format!("[ebp - {}]", self.frame_size - offset)
+    }
+}
+
 enum Val {
-    // Accumulator contains the value itself
-    Accum,
-    // Accumulator contains a pointer to the value
-    AccumRef,
+    // Immediate integer value
+    Immed(usize),
+    // Integer (or pointer) value in a register
+    Reg(Reg),
     // Value is stored on the stack
     Stack(usize),
     // Value is stored in static storage
     Static(Rc<str>, usize),
+    // Dereference of a pointer
+    Deref(Box<Val>, usize),
 }
 
-// Load a reference to a value into the accumulator
-// Aka for making a pointer to a value
-fn ref_to_accum<T: std::io::Write>(frame_size: usize, accum: &str, val: &Val, output: &mut T) {
+// Generate pointer creation code
+fn gen_ref<T: std::io::Write>(fctx: &mut FuncCtx, val: Val, output: &mut T) -> Val {
     match val {
-        Val::Accum => panic!("Rvalue used as lvalue"),
-        Val::AccumRef => (), // Already have the address (only ends up here when derefing a pointer we just ref'd)
-        Val::Stack(offset) => { print_or_die!(output, "lea {}, [rbp - {}]", accum, frame_size - offset); },
-        Val::Static(ident, offset) => { print_or_die!(output, "lea {}, [{} + {}]", accum, ident, offset); },
+        Val::Stack(offset) => {
+            // FIXME: temporaries won't necessarily fit into registers
+            let reg = fctx.regmask.alloc_reg().unwrap();
+            print_or_die!(output, "lea {}, [ebp - {}]",
+                                    reg.to_str(), fctx.frame_size - offset);
+            Val::Reg(reg)
+        },
+        Val::Static(name, offset) => {
+            let reg = fctx.regmask.alloc_reg().unwrap();
+            print_or_die!(output, "lea {}, [{} + {}]",
+                                    reg.to_str(), name, offset);
+            Val::Reg(reg)
+
+        },
+        _ => panic!("Cannot take address of rvalue!"),
     }
 }
 
-// Load a value into the accumulator
-fn val_to_accum<T: std::io::Write>(frame_size: usize, accum: &str, val: &Val, output: &mut T) {
+// Create value at an offset from an lvalue
+fn add_offs(val: Val, add_offset: usize) -> Val {
     match val {
-        Val::Accum => (), // Already have the value
-        Val::AccumRef => { print_or_die!(output, "mov {}, [{}]", accum, accum); }, // Load value from pointer
-        Val::Stack(offset) => { print_or_die!(output, "mov {}, [rbp - {}]", accum, frame_size - offset); }, // Load value from stack
-        Val::Static(ident, offset) => { print_or_die!(output, "mov {}, [{} + {}]", accum, ident, offset); }, // Load value from static
+        Val::Stack(offset) => Val::Stack(offset + add_offset),
+        Val::Static(name, offset) => Val::Static(name, offset + add_offset),
+        Val::Deref(derefed_val, offset) => Val::Deref(derefed_val, offset + add_offset),
+        _ => panic!("Cannot take offset from rvalue!"),
     }
 }
 
-// Make a new value that refers to an offset from a previous value
-fn field_val<T: std::io::Write>(accum: &str, val: &Val, field_offs: usize, output: &mut T) -> Val {
-    match val {
-        Val::Accum => panic!("Rvalue used as lvalue"),
-        Val::AccumRef => { print_or_die!(output, "add {}, {}", accum, field_offs); Val::AccumRef },
-        Val::Stack(offset) => Val::Stack(offset + field_offs),
-        Val::Static(ident, offset) => Val::Static(ident.clone(), offset + field_offs),
+// Save all caller-saved registers before a call
+fn pushcallersaved<T: std::io::Write>(usedregs: u16, output: &mut T) -> (u16, bool) {
+    let mut savemask = 0u16;
+    let mut pad = false;
+    for reg in CALLER_SAVED {
+        let regmask = reg as u16;
+        if usedregs & regmask != 0 {
+            savemask |= regmask;
+            pad = !pad;
+            print_or_die!(output, "push {}", reg.to_str());
+        }
+    }
+    if pad {
+        // Make sure stack alingment is preserved
+        print_or_die!(output, "push 0");
+    }
+    (savemask, pad)
+}
+
+// Restore saved caller-saved registers
+fn popcallersaved<T: std::io::Write>(savemask: u16, pad: bool, output: &mut T) {
+    if pad {
+        // Remove padding if present
+        print_or_die!(output, "add rsp, 8");
+    }
+
+    for reg in CALLER_SAVED.iter().rev() {
+        let regmask = *reg as u16;
+        if savemask & regmask != 0 {
+            print_or_die!(output, "pop {}", reg.to_str());
+        }
     }
 }
+
 
 fn gen_expr<T: std::io::Write>(
         file: &File,
-        accum: &str,
+        fctx: &mut FuncCtx,
         in_expr: &Expr,
-        frame_size: usize,
-        locals: &HashMap<Rc<str>, (&Type, usize)>,
         output: &mut T) -> (Type, Val) {
 
     match in_expr {
-        Expr::U8(v)  => { print_or_die!(output, "mov {}, {}", accum, v); (Type::U8, Val::Accum) },
-        Expr::I8(v)  => { print_or_die!(output, "mov {}, {}", accum, v); (Type::I8, Val::Accum) },
-        Expr::U16(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::U16, Val::Accum) },
-        Expr::I16(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::I16, Val::Accum) },
-        Expr::U32(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::U32, Val::Accum) },
-        Expr::I32(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::I32, Val::Accum) },
-        Expr::U64(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::U64, Val::Accum) },
-        Expr::I64(v) => { print_or_die!(output, "mov {}, {}", accum, v); (Type::I64, Val::Accum) },
+        Expr::U8(v)  => (Type::U8, Val::Immed(*v as usize)),
+        Expr::I8(v)  => (Type::I8, Val::Immed(*v as usize)),
+        Expr::U16(v) => (Type::U16, Val::Immed(*v as usize)),
+        Expr::I16(v) => (Type::I16, Val::Immed(*v as usize)),
+        Expr::U32(v) => (Type::U32, Val::Immed(*v as usize)),
+        Expr::I32(v) => (Type::I32, Val::Immed(*v as usize)),
+        Expr::U64(v) => (Type::U64, Val::Immed(*v as usize)),
+        Expr::I64(v) => (Type::I64, Val::Immed(*v as usize)),
 
         Expr::Ident(ident) => {
-            if let Some((dtype, offset)) = locals.get(ident) {
-                ((*dtype).clone(), Val::Stack(*offset))
+            if let Some((dtype, offset)) = fctx.locals.get(ident) {
+                (dtype.clone(), Val::Stack(*offset))
             } else if let Some(s) = file.statics.get(ident) {
                 (s.dtype.clone(), Val::Static(ident.clone(), 0))
             } else {
@@ -136,42 +269,42 @@ fn gen_expr<T: std::io::Write>(
         },
 
         Expr::Ref(expr) => {
-            let (src_type, src_val) = gen_expr(file, accum, expr, frame_size, locals, output);
-            ref_to_accum(frame_size, accum, &src_val, output);
-            (Type::Ptr { base_type: Box::from(src_type.clone()) }, Val::Accum)
+            let (src_type, src_val) = gen_expr(file, fctx, expr, output);
+            (Type::Ptr { base_type: Box::from(src_type.clone()) },
+                gen_ref(fctx, src_val, output))
         },
 
         Expr::Deref(expr) => {
-            let (src_type, src_val) = gen_expr(file, accum, expr, frame_size, locals, output);
+            let (src_type, src_val) = gen_expr(file, fctx, expr, output);
             match src_type {
-                // Read the pointer's value into the accumulator, than change
-                // the value type to to reflect this state
-                Type::Ptr { base_type } => {
-                    val_to_accum(frame_size, accum, &src_val, output);
-                    ((*base_type).clone(), Val::AccumRef)
-                },
-                // In case of an array we just change the type
-                Type::Array { elem_type, .. } => ((*elem_type).clone(), src_val),
+                // Create dereferenced value from pointer
+                Type::Ptr { base_type } =>
+                    ((*base_type).clone(), Val::Deref(Box::from(src_val), 0)),
+                // Create new value with the array's element type at the same location
+                // (offset was already added to the array's value if this deref was
+                // de-sugared from array indexing)
+                Type::Array { elem_type, .. } =>
+                    ((*elem_type).clone(), src_val),
                 _ => panic!("Can't dereference non-reference type"),
             }
         }
 
         Expr::Field(expr, ident) => {
-            let (src_type, src_val) = gen_expr(file, accum, expr, frame_size, locals, output);
+            let (src_type, src_val) = gen_expr(file, fctx, expr, output);
             match src_type {
                 Type::Record(record) => {
                     if let Some((field_type, field_offset)) = record.fields.get(ident) {
-                        (field_type.clone(),
-                            field_val(accum, &src_val, *field_offset, output))
+                        (field_type.clone(), add_offs(src_val, *field_offset))
                     } else {
                         panic!("Non-existent field {} accessed", ident);
                     }
                 },
-                _ => panic!("Field access on non-record type"),
+                _ => panic!("Can't access field of non-record type"),
             }
         },
 
-        Expr::Call(expr, ref params) => {
+        Expr::Call(expr, params) => {
+            // Find target function to call
             let func = match &**expr {
                 Expr::Ident(ident) => {
                     if let Some(func) = file.funcs.get(ident) {
@@ -182,25 +315,29 @@ fn gen_expr<T: std::io::Write>(
                 },
                 _ => panic!("Function name must be an identifier"),
             };
+            // Save callee saved registers
+            let (savemask, pad) = pushcallersaved(fctx.regmask.usedregs, output);
+
             for (i, param) in params.iter().enumerate() {
-                match i {
-                    0 => { let (_, val) = gen_expr(file, "rdi", param, frame_size, locals, output); val_to_accum(frame_size, "rdi", &val, output) },
-                    1 => { let (_, val) = gen_expr(file, "rsi", param, frame_size, locals, output); val_to_accum(frame_size, "rsi", &val, output) },
-                    2 => { let (_, val) = gen_expr(file, "rdx", param, frame_size, locals, output); val_to_accum(frame_size, "rdx", &val, output) },
-                    3 => { let (_, val) = gen_expr(file, "rcx", param, frame_size, locals, output); val_to_accum(frame_size, "rcx", &val, output) },
-                    4 => { let (_, val) = gen_expr(file, "r8", param, frame_size, locals, output); val_to_accum(frame_size, "r8", &val, output) },
-                    5 => { let (_, val) = gen_expr(file, "r9", param, frame_size, locals, output); val_to_accum(frame_size, "r9", &val, output) },
-                    _ => panic!("FIXME: too many call params"),
-                };
+                // FIXME: support more than 6 parameters
+                let param_reg = PARAM_REGS[i];
+                let (param_type, param_val) = gen_expr(file, fctx, param, output);
             }
             if func.varargs {
-                // Varargs functions need to be told the number of float arguments
+                // NOTE: Varargs functions need to be told the number of
+                // floating-point arguments
                 print_or_die!(output, "xor rax, rax");
             }
             print_or_die!(output, "call {}", func.name);
-            (func.rettype.clone(), Val::Accum)
+
+            // Restore registers
+            popcallersaved(savemask, pad, output);
+
+            // FIXME: rax might be overwritten above
+            (func.rettype.clone(), Val::Reg(Reg::Rax))
         },
 
+/*
         Expr::Inv(ref expr) => {
             let (src_type, src_val) = gen_expr(file, accum, expr, frame_size, locals, output);
             assert_integral(&src_type);
@@ -237,7 +374,7 @@ fn gen_expr<T: std::io::Write>(
             (lhs_type, Val::Accum)
         },
 
-        /*Expr::Sub(ref expr1, ref expr2) => {
+        Expr::Sub(ref expr1, ref expr2) => {
             gen_expr(expr1, locals, data);
             gen_expr(expr2, locals, data);
         },
@@ -282,65 +419,54 @@ fn gen_expr<T: std::io::Write>(
     }
 }
 
-fn store_accum_to_val<T: std::io::Write>(accum: &str, val: &Val, output: &mut T) {
-    match val {
-        Val::Accum => panic!("Write to rvalue"),
-        Val::AccumRef => { print_or_die!(output, "mov [r10], {}", accum); }, // NOTE: Awful hack r10 is the dest's accum
-        Val::Stack(offset) => { print_or_die!(output, "mov [rsp + {}], {}", offset, accum); },
-        Val::Static(ident, offset) => { print_or_die!(output, "mov [{} + {}], {}", ident, offset, accum); },
-    }
-}
-
 fn gen_func<T: std::io::Write>(file: &File, func: &Func, output: &mut T) {
     print_or_die!(output, "{}:", func.name);
 
-    // Generate locals
-    let mut frame_size = 0usize;
+    // Create context
+    let mut ctx = FuncCtx {
+        regmask: RegMask::new(),
+        locals: HashMap::new(),
+        frame_size: 0,
+    };
 
-    let mut locals = HashMap::new();
+    // Allocate stack slots for parameters
+    // We note than the param to offset map for later use
     let mut params = Vec::new();
-
     for (i, (name, t)) in func.params.iter().enumerate() {
         if t.get_size() > 8 {
             panic!("FIXME: larger-than register parameters!");
         }
         // Add local variable for parameter
-        locals.insert(name.clone(), (t, frame_size));
+        ctx.locals.insert(name.clone(), (t.clone(), ctx.frame_size));
         // Save index to stack offset map
-        params.push((i, frame_size));
+        params.push((i, ctx.frame_size));
         // Increase stack frame size
         // HACK!: assume all parameters are 8 bytes for simpler moves
-        frame_size += 8;
+        ctx.frame_size += 8;
     }
 
-    // Allocate stack frame for all auto variables too
+    // Allocate stack slots for auto variables
     for stmt in &func.stmts {
         match stmt {
-            Stmt::Auto(name, t, _) => {
-                locals.insert(name.clone(), (t, frame_size));
-                frame_size += t.get_size();
+            Stmt::Auto(name, dtype, _) => {
+                ctx.locals.insert(name.clone(), (dtype.clone(), ctx.frame_size));
+                ctx.frame_size += dtype.get_size();
             },
             _ => (),
         }
     }
 
-    // Align to a multiple of 16, then add 8 (sysv abi)
-    frame_size = (frame_size + 15) / 16 * 16 + 8;
-
+    // Align frame size to a multiple of 16, then add 8 (sysv abi)
+    ctx.frame_size = (ctx.frame_size + 15) / 16 * 16 + 8;
     // Generate function prologue
-    print_or_die!(output, "push rbp\nmov rbp, rsp\nsub rsp, {}", frame_size);
+    print_or_die!(output, "push rbp\nmov rbp, rsp\nsub rsp, {}", ctx.frame_size);
 
     // Move parameters from registers to stack
-    for param in params {
-        match param {
-            (0, offset) => { print_or_die!(output, "mov qword [rbp - {}], rdi", frame_size - offset); },
-            (1, offset) => { print_or_die!(output, "mov qword [rbp - {}], rsi", frame_size - offset); },
-            (2, offset) => { print_or_die!(output, "mov qword [rbp - {}], rdx", frame_size - offset); },
-            (3, offset) => { print_or_die!(output, "mov qword [rbp - {}], rcx", frame_size - offset); },
-            (4, offset) => { print_or_die!(output, "mov qword [rbp - {}], r8", frame_size - offset); },
-            (5, offset) => { print_or_die!(output, "mov qword [rbp - {}], r9", frame_size - offset); },
-            _ => panic!("FIXME: more than six parameters!"),
-        }
+    for (i, offset) in params {
+        // FIXME: support more than 6 parameters
+        print_or_die!(output, "mov qword [rbp - {}], {}",
+                ctx.frame_size - offset,
+                PARAM_REGS[i].to_str());
     }
 
     for stmt in &func.stmts {
@@ -348,15 +474,15 @@ fn gen_func<T: std::io::Write>(file: &File, func: &Func, output: &mut T) {
             Stmt::Label(label) => {
                 print_or_die!(output, ".{}:", label);
             },
-            Stmt::Auto(ident, dtype, init) => {},
+            /*Stmt::Auto(ident, dtype, init) => {},
             Stmt::Set(ref dest, ref expr) => {
                 let (ltype, lval) = gen_expr(file, "r10", dest, frame_size, &locals, output);
                 let (rtype, rval) = gen_expr(file, "rax", expr, frame_size, &locals, output);
                 val_to_accum(frame_size, "rax", &rval, output);
                 store_accum_to_val("rax", &lval, output);
-            }
+            }*/
             Stmt::Eval(ref expr) => {
-                gen_expr(file, "rax", expr, frame_size, &locals, output);
+                gen_expr(file, &mut ctx, expr, output);
             },
             Stmt::Jmp(label) => {
                 print_or_die!(output, "jmp .{}", label);
