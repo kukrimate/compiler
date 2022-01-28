f69052a2eb1786fc48e9c5ebfccdc19d2b0c27de
Mate Kukri <km@mkukri.xyz>
2022-01-28 22:29:44 +0000

    Replace logos with a hand-written lexer
    This commit also introduces a variety of Unicode cleanliness patches.

--- src/lex.rs
+++ src/lex.rs
@@ -4,219 +4,362 @@
 // Lexical analyzer using logos
 //
 
-use logos;
+use std::collections::HashMap;
 use std::rc::Rc;
 
-// Exposed lexer type
-pub type Lexer<'source> = logos::Lexer<'source, Token>;
-
-fn bin(lex: &mut Lexer) -> usize {
-    let mut result = 0usize;
-    for ch in &lex.slice().as_bytes()[2..] {
-        result = (result << 1) + (ch - b'0') as usize;
-    }
-    result
-}
-
-fn oct(lex: &mut Lexer) -> usize {
-    let mut result = 0usize;
-    for ch in &lex.slice().as_bytes()[2..] {
-        result = (result << 3) + (ch - b'0') as usize;
-    }
-    result
-}
-
-fn dec(lex: &mut Lexer) -> usize {
-    let mut result = 0usize;
-    for ch in lex.slice().as_bytes() {
-        result = result * 10 + (ch - b'0') as usize;
-    }
-    result
-}
-
-fn hex(lex: &mut Lexer) -> usize {
-    let mut result = 0usize;
-    for ch in &lex.slice().as_bytes()[2..] {
-        match ch {
-            b'0'..=b'9' => {
-                result = (result << 4) + (ch - b'0') as usize;
-            },
-            b'a'..=b'f' => {
-                result = (result << 4) + (ch - b'a' + 0xa) as usize;
-            },
-            b'A'..=b'F' => {
-                result = (result << 4) + (ch - b'A' + 0xa) as usize;
-            }
-            _ => panic!(),
-        }
-    }
-    result
-}
-
-fn ch(lex: &mut Lexer) -> usize {
-    // FIXME: parse escape sequences
-    let mut result = 0usize;
-    for ch in lex.slice().as_bytes() {
-        result = result << 8 | *ch as usize;
-    }
-    result
-}
-
-fn str(lex: &mut Lexer) -> Rc<str> {
-    // FIXME: parse escape sequences
-    let s = String::from(lex.slice()).replace(r"\n", "\n");
-    s[1..s.len() - 1].into()
-}
-
-fn ident(lex: &mut Lexer) -> Rc<str> {
-    lex.slice().into()
-}
-
-#[derive(logos::Logos, Debug)]
+#[derive(Clone,Debug)]
 pub enum Token {
     // Typenames
-    #[token("bool")]
     Bool,
-    #[token("u8")]
     U8,
-    #[token("i8")]
     I8,
-    #[token("u16")]
     U16,
-    #[token("i16")]
     I16,
-    #[token("u32")]
     U32,
-    #[token("i32")]
     I32,
-    #[token("u64")]
     U64,
-    #[token("i64")]
     I64,
-    #[token("usize")]
     USize,
 
     // Boolean constants
-    #[token("true")]
     True,
-    #[token("false")]
     False,
 
     // Declarations
-    #[token("record")]
     Record,
-    #[token("fn")]
     Fn,
-    #[token("static")]
     Static,
-    #[token("let")]
     Let,
 
     // Declaration markers
-    #[token("export")]
     Export,
-    #[token("extern")]
     Extern,
 
     // Constants
-    #[regex(r"0b[0-1]+", bin)]
-    #[regex(r"0o[0-7]+", oct)]
-    #[regex(r"[0-9]+", dec)]
-    #[regex(r"0x[0-9a-fA-F]+", hex)]
-    #[regex(r"'(\\.|[^\\'])*'", ch)]
     Constant(usize),
-
-    #[regex(r#""(\\.|[^\\"])*""#, str)]
     Str(Rc<str>),
 
     // Indentifiers
-    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", ident)]
     Ident(Rc<str>),
 
     // Symbols
-    #[token("=")]
     Assign,    // =
-    #[token("~")]
     Tilde,     // ~
-    #[token("!")]
     Excl,      // !
-    #[token("*")]
     Mul,       // *
-    #[token("/")]
     Div,       // /
-    #[token("%")]
     Rem,       // %
-    #[token("+")]
     Add,       // +
-    #[token("-")]
     Sub,       // -
-    #[token("<<")]
     Lsh,       // <<
-    #[token(">>")]
-    Rsh,       // >>
-    #[token("<")]
+    Rsh,       // >
     Lt,        // <
-    #[token("<=")]
     Le,        // <=
-    #[token(">")]
     Gt,        // >
-    #[token(">=")]
     Ge,        // >=
-    #[token("==")]
     Eq,        // ==
-    #[token("!=")]
     Ne,        // !=
-    #[token("&")]
     And,       // &
-    #[token("^")]
     Xor,       // ^
-    #[token("|")]
     Or,        // |
-    #[token("&&")]
     LAnd,      // &&
-    #[token("||")]
     LOr,       // ||
-    #[token("(")]
     LParen,    // (
-    #[token(")")]
     RParen,    // )
-    #[token("[")]
     LSq,       // [
-    #[token("]")]
     RSq,       // ]
-    #[token("{")]
     LCurly,    // {
-    #[token("}")]
     RCurly,    // }
-    #[token(";")]
     Semicolon, // ;
-    #[token(":")]
     Colon,     // :
-    #[token(",")]
     Comma,     // ,
-    #[token(".")]
     Dot,       // .
-    #[token("as")]
     As,        // cast operator
-    #[token("->")]
     Arrow,     // function return type
-    #[token("...")]
     Varargs,   // variable arguments marker
 
     // Statements
-    #[token("ret")]
     Ret,
-    #[token("jmp")]
     Jmp,
-    #[token("if")]
     If,
-    #[token("else")]
     Else,
-    #[token("while")]
     While,
 
-    #[error]
-    #[regex(r"[ \t\n\f\v]+", logos::skip)]
-    #[regex(r"//.*\n", logos::skip)]
-    Error,
+    Error(u8),
+}
+
+pub struct Lexer<'a> {
+    kws: HashMap<&'a str, Token>,
+    data: &'a [u8],
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(data: &'a str) -> Lexer<'a> {
+        let mut kws = HashMap::new();
+        kws.insert("bool",      Token::Bool);
+        kws.insert("u8",        Token::U8);
+        kws.insert("i8",        Token::I8);
+        kws.insert("u16",       Token::U16);
+        kws.insert("i16",       Token::I16);
+        kws.insert("u32",       Token::U32);
+        kws.insert("i32",       Token::I32);
+        kws.insert("u64",       Token::U64);
+        kws.insert("i64",       Token::I64);
+        kws.insert("usize",     Token::USize);
+        kws.insert("true",      Token::True);
+        kws.insert("false",     Token::False);
+        kws.insert("record",    Token::Record);
+        kws.insert("fn",        Token::Fn);
+        kws.insert("static",    Token::Static);
+        kws.insert("let",       Token::Let);
+        kws.insert("export",    Token::Export);
+        kws.insert("extern",    Token::Extern);
+        kws.insert("as",        Token::As);
+        kws.insert("ret",       Token::Ret);
+        kws.insert("jmp",       Token::Jmp);
+        kws.insert("if",        Token::If);
+        kws.insert("else",      Token::Else);
+        kws.insert("while",     Token::While);
+        Lexer {
+            kws: kws,
+            data: data.as_bytes()
+        }
+    }
+
+    fn look(&mut self, i: usize) -> Option<u8> {
+        self.data.get(i).cloned()
+    }
+
+    fn eat(&mut self, n: usize) {
+        self.data = &self.data[n..]
+    }
+
+    fn unescape(&mut self) -> char {
+        match self.look(0).expect("Missing escape sequence") {
+            b'n' => { self.eat(1); '\n' }
+            b'r' => { self.eat(1); '\r' }
+            b't' => { self.eat(1); '\t' }
+            b'0' => { self.eat(1); '\0' }
+            b'x' | b'u' => {
+                let mut val = 0;
+                self.eat(1);
+                loop {
+                    match self.look(0) {
+                        Some(byte @ (b'0'..=b'9')) => val = val << 4 | (byte - b'0') as u32,
+                        Some(byte @ (b'a'..=b'f')) => val = val << 4 | (byte - b'a' + 0xa) as u32,
+                        Some(byte @ (b'A'..=b'F')) => val = val << 4 | (byte - b'A' + 0xa) as u32,
+                        _ => break,
+                    }
+                    self.eat(1);
+                }
+                std::char::from_u32(val).expect("Escape sequence must be valid unicode")
+            },
+            _ => panic!("Unknown escape sequence"),
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        Some(loop {
+            break match self.look(0)? {
+                // Whitespace
+                b' ' | b'\n' | b'\r' | b'\t' => { self.eat(1); continue },
+                // Identifier
+                b'_' | b'a'..=b'z' | b'A'..=b'Z' => {
+                    let begin = self.data;
+
+                    // First character already matched
+                    self.eat(1);
+
+                    // Consume characters until a non-matching one is hit
+                    while let Some(b'_' | b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9') = self.look(0) {
+                        self.eat(1);
+                    }
+
+                    let slice = unsafe { std::str::from_utf8_unchecked(&begin[.. begin.len() - self.data.len()]) };
+                    if let Some(tok) = self.kws.get(slice) {
+                        tok.clone()
+                    } else {
+                        Token::Ident(slice.into())
+                    }
+                },
+                // Numbers
+                b'0'..=b'9' => {
+                    let mut val = 0;
+                    match self.look(1) {
+                        Some(b'b') => {
+                            self.eat(2);
+                            loop {
+                                match self.look(0) {
+                                    Some(byte @ (b'0'..=b'1')) => val = val << 1 | (byte - b'0') as usize,
+                                    _ => break,
+                                }
+                                self.eat(1);
+                            }
+                        },
+                        Some(b'o') => {
+                            self.eat(2);
+                            loop {
+                                match self.look(0) {
+                                    Some(byte @ (b'0'..=b'7')) => val = val << 3 | (byte - b'0') as usize,
+                                    _ => break,
+                                }
+                                self.eat(1);
+                            }
+                        },
+                        Some(b'x') => {
+                            self.eat(2);
+                            loop {
+                                match self.look(0) {
+                                    Some(byte @ (b'0'..=b'9')) => val = val << 4 | (byte - b'0') as usize,
+                                    Some(byte @ (b'a'..=b'f')) => val = val << 4 | (byte - b'a' + 0xa) as usize,
+                                    Some(byte @ (b'A'..=b'F')) => val = val << 4 | (byte - b'A' + 0xa) as usize,
+                                    _ => break,
+                                }
+                                self.eat(1);
+                            }
+                        },
+                        _ => {
+                            loop {
+                                match self.look(0) {
+                                    Some(byte @ (b'0'..=b'9')) => val = val * 10 + (byte - b'0') as usize,
+                                    _ => break,
+                                }
+                                self.eat(1);
+                            }
+                        }
+                    }
+                    Token::Constant(val)
+                },
+                // Character constant
+                b'\'' => {
+                    let mut v = Vec::new();
+                    self.eat(1);
+                    loop {
+                        match self.look(0) {
+                            Some(b'\\') => {
+                                self.eat(1);
+                                // NOTE: the part before the escape must be valid
+                                // UTF-8, thus we can do this hackery
+                                let mut s = unsafe { String::from_utf8_unchecked(v) };
+                                s.push(self.unescape());
+                                v = s.into_bytes();
+                            },
+                            Some(b'\'')  => {
+                                self.eat(1);
+                                break;
+                            },
+                            Some(byte)  => {
+                                self.eat(1);
+                                v.push(byte);
+                            },
+                            None        => panic!("Unterminated char constant"),
+                        }
+                    }
+                    let s = unsafe { String::from_utf8_unchecked(v) };
+                    let mut chars = s.chars();
+                    let t = Token::Constant(chars.next().expect("Empty char constant") as usize);
+                    if let Some(_) = chars.next() {
+                        panic!("Char constant must contain only one codepoint")
+                    }
+                    t
+                },
+                // String literal
+                b'"' => {
+                    let mut v = Vec::new();
+                    self.eat(1);
+                    loop {
+                        match self.look(0) {
+                            Some(b'\\') => {
+                                self.eat(1);
+                                // NOTE: the part before the escape must be valid
+                                // UTF-8, thus we can do this hackery
+                                let mut s = unsafe { String::from_utf8_unchecked(v) };
+                                s.push(self.unescape());
+                                v = s.into_bytes();
+                            },
+                            Some(b'"')  => {
+                                self.eat(1);
+                                break;
+                            },
+                            Some(byte)  => {
+                                self.eat(1);
+                                v.push(byte);
+                            },
+                            None        => panic!("Unterminated string literal"),
+                        }
+                    }
+                    Token::Str(unsafe { String::from_utf8_unchecked(v) }.into())
+                },
+                // Symbols
+                b'.' => match (self.look(1), self.look(2)) {
+                    (Some(b'.'), Some(b'.')) => { self.eat(3); Token::Varargs },
+                    _                        => { self.eat(1); Token::Dot },
+                },
+                b'=' => match self.look(1) {
+                    Some(b'=') => { self.eat(2); Token::Eq },
+                    _          => { self.eat(1); Token::Assign },
+                },
+                b'!' => match self.look(1) {
+                    Some(b'=') => { self.eat(2); Token::Ne },
+                    _          => { self.eat(1); Token::Excl },
+                },
+                b'<' => match self.look(1) {
+                    Some(b'<') => { self.eat(2); Token::Lsh },
+                    Some(b'=') => { self.eat(2); Token::Le },
+                    _          => { self.eat(1); Token::Lt },
+                },
+                b'>' => match self.look(1) {
+                    Some(b'>') => { self.eat(2); Token::Rsh },
+                    Some(b'=') => { self.eat(2); Token::Ge },
+                    _          => { self.eat(1); Token::Gt },
+                },
+                b'&' => match self.look(1) {
+                    Some(b'&') => { self.eat(2); Token::LAnd },
+                    _          => { self.eat(1); Token::And },
+                },
+                b'|' => match self.look(1) {
+                    Some(b'|') => { self.eat(2); Token::LOr },
+                    _          => { self.eat(1); Token::Or },
+                },
+                b'-' => match self.look(1) {
+                    Some(b'>') => { self.eat(2); Token::Arrow },
+                    _          => { self.eat(1); Token::Sub },
+                },
+                b'/' => match self.look(1) {
+                    Some(b'/') => {
+                        let mut n = 2;
+                        loop {
+                            if let Some(b'\n') | None = self.look(n) {
+                                n += 1;
+                                break
+                            }
+                            n += 1;
+                        }
+                        self.eat(n);
+                        continue;
+                    },
+                    _          => { self.eat(1); Token::Div },
+                },
+                b'~' => { self.eat(1); Token::Tilde },
+                b'*' => { self.eat(1); Token::Mul },
+                b'%' => { self.eat(1); Token::Rem },
+                b'+' => { self.eat(1); Token::Add },
+                b'^' => { self.eat(1); Token::Xor },
+                b'(' => { self.eat(1); Token::LParen },
+                b')' => { self.eat(1); Token::RParen },
+                b'[' => { self.eat(1); Token::LSq },
+                b']' => { self.eat(1); Token::RSq },
+                b'{' => { self.eat(1); Token::LCurly },
+                b'}' => { self.eat(1); Token::RCurly },
+                b';' => { self.eat(1); Token::Semicolon },
+                b':' => { self.eat(1); Token::Colon },
+                b',' => { self.eat(1); Token::Comma },
+                byte => { self.eat(1); Token::Error(byte) },
+            };
+        })
+    }
 }
